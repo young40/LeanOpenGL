cd1fa1081c39191b29c2c6beac1789c48c4ea06a
Peter Young <young40@gmail.com>
2015-04-02 00:11:58 +0800

    add tutorial 04

--- Tutorial_01/T1.fs
+++ Tutorial_01/T1.fs
@@ -2,7 +2,11 @@
 
 out vec3 color;
 
+in vec3 fragmentColor;
+
+
 void main()
 {
-    color = vec3(1, 0, 0);
+    color = fragmentColor;
+    
 }
--- Tutorial_01/Tutorial_01/main.cpp
+++ Tutorial_01/Tutorial_01/main.cpp
@@ -57,19 +57,53 @@ int main(int argc, const char * argv[]) {
     glfwMakeContextCurrent(window);
     
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
     
     
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
     
-    
     static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f,  -1.0f,  0.0f,
-        1.0f,   -1.0f,  0.0f,
-        0.0f,   1.0f,   0.0f,
-        
+        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+        -1.0f,-1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f, // triangle 1 : end
+        1.0f, 1.0f,-1.0f, // triangle 2 : begin
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f, // triangle 2 : end
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        -1.0f,-1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        -1.0f,-1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f,-1.0f,
+        1.0f,-1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f,-1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f,-1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f,
+        -1.0f, 1.0f, 1.0f,
+        1.0f,-1.0f, 1.0f
     };
+    
     
     GLuint vertexBuffer;
     
@@ -91,7 +125,7 @@ int main(int argc, const char * argv[]) {
     mat4 Projection = glm::perspective(45.0f, 4.0f/3.0f, 0.1f, 100.0f);
 //    mat4 Projection = glm::ortho(-100, 100, 100, -100)
     
-    mat4 View = glm::lookAt(vec3(4, 3, 3),
+    mat4 View = glm::lookAt(vec3(4, 3, -3),
                             vec3(0, 0, 0),
                             vec3(0, 1, 0));
     
@@ -99,24 +133,71 @@ int main(int argc, const char * argv[]) {
     
     mat4 MVP = Projection * View * Model;
     
+    static const GLfloat g_color_buffer_data[] = {
+        0.583f, 0.771f, 0.014f,
+        0.609f, 0.115f, 0.436f,
+        0.327f, 0.483f, 0.844f,
+        0.822f, 0.569f, 0.201f,
+        0.435f, 0.602f, 0.223f,
+        0.310f, 0.747f, 0.185f,
+        0.597f, 0.770f, 0.761f,
+        0.559f, 0.436f, 0.730f,
+        0.359f, 0.583f, 0.152f,
+        0.483f, 0.596f, 0.789f,
+        0.559f, 0.861f, 0.639f,
+        0.195f, 0.548f, 0.859f,
+        0.014f, 0.184f, 0.576f,
+        0.771f, 0.328f, 0.970f,
+        0.406f, 0.615f, 0.116f,
+        0.676f, 0.977f, 0.133f,
+        0.971f, 0.572f, 0.833f,
+        0.140f, 0.616f, 0.489f,
+        0.997f, 0.513f, 0.064f,
+        0.945f, 0.719f, 0.592f,
+        0.543f, 0.021f, 0.978f,
+        0.279f, 0.317f, 0.505f,
+        0.167f, 0.620f, 0.077f,
+        0.347f, 0.857f, 0.137f,
+        0.055f, 0.953f, 0.042f,
+        0.714f, 0.505f, 0.345f,
+        0.783f, 0.290f, 0.734f,
+        0.722f, 0.645f, 0.174f,
+        0.302f, 0.455f, 0.848f,
+        0.225f, 0.587f, 0.040f,
+        0.517f, 0.713f, 0.338f,
+        0.053f, 0.959f, 0.120f,
+        0.393f, 0.621f, 0.362f,
+        0.673f, 0.211f, 0.457f,
+        0.820f, 0.883f, 0.371f,
+        0.982f, 0.099f, 0.879f
+    };
+    
+    GLuint colorBuffer;
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    
     
     
     while (!glfwWindowShouldClose(window)) {
 //        glfwSwapBuffers(window);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //开启深度测试后要没帧清理深度buffer
         glUseProgram(programID);
         
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         
         glEnableVertexAttribArray(0);
-        
         glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-        
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
         
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 12*3);
         
         glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
         
         
         
