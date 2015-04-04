ac60022c8244b2b2c8b5c0b8a2182a8f7989c2d2
Peter Young <young40@gmail.com>
2015-04-04 21:51:37 +0800

    add new Texture Test Project

---
+++ TextureFirstTest/TextureFirstTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TextureFirstTest
+//
+//  Created by Young40 on 4/4/15.
+//  Copyright (c) 2015 Young40 Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
