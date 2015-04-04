0c0c68398dee54dc7aa816f7e7630b29e49058c9
Peter Young <young40@gmail.com>
2015-04-04 21:58:17 +0800

    add libs

---
+++ includes/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/young40/Downloads/glm/glm
+# Build directory: /Users/young40/Downloads/glm/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
