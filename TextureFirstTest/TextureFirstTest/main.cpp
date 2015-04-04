8b5db2ffa35149c31e321206b390bead023a6177
Peter Young <young40@gmail.com>
2015-04-04 23:32:10 +0800

    update text

--- TextureFirstTest/TextureFirstTest/main.cpp
+++ TextureFirstTest/TextureFirstTest/main.cpp
@@ -7,7 +7,10 @@
 //
 
 #include <iostream>
+
+#include <OpenGL/gl3.h>
 #include <GLFW/glfw3.h>
+#include <common/shader.hpp>
 
 extern "C"
 {
@@ -40,10 +43,41 @@ int main(int argc, const char * argv[]) {
     
     glfwMakeContextCurrent(window);
     
+    glClearColor(0.3f, 0.2f, 0.4f, 0.0f);
+    
+    GLuint vertexArrayID;
+    glGenVertexArrays(1, &vertexArrayID);
+    glBindVertexArray(vertexArrayID);
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,  -1.0f,  0.0f,
+        1.0f,   -1.0f,  0.0f,
+        0.0f,   1.0f,   0.0f,
+    };
+    
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
+    
+    
+    
+    
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(
+                              0,
+                              3,
+                              GL_FLOAT,
+                              GL_FALSE,
+                              0,
+                              (void*)0);
         
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
