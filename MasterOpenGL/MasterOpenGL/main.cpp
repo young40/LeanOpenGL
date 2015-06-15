0c6999cc62f988a2876a53e3529a8be6bfd92600
Peter Young <young40@gmail.com>
2015-06-15 22:42:37 +0800

    init Xcode project

---
+++ MasterOpenGL/MasterOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MasterOpenGL
+//
+//  Created by Peter Young on 6/15/15.
+//  Copyright (c) 2015 Peter Young. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
